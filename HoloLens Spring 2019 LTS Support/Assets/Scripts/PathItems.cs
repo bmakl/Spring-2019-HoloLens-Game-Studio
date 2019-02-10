5312fb6de66a8866e8fce748ae512a96d9167cf9
Ben Rubenstein <ben24112411@gmail.com>
2019-02-10 14:57:11 -0500

    Added placeable path items

--- HoloLens Spring 2019 LTS Support/Assets/Scripts/PathItems.cs
+++ HoloLens Spring 2019 LTS Support/Assets/Scripts/PathItems.cs
@@ -3,20 +3,47 @@ using System.Collections.Generic;
 using UnityEngine;
 using HoloToolkit.Unity.InputModule;
 
+[RequireComponent(typeof(BoxCollider))]
 public class PathItems : MonoBehaviour, IInputClickHandler
 {
-    [SerializeField] GameObject barrier;
+    [SerializeField] GameObject fence;
+    [SerializeField] float placeCoolDown;
+    private bool canPlace = true;
+
 
     public void OnInputClicked(InputClickedEventData eventData)
     {
-        if (GazeManager.Instance.HitInfo.collider != null)
+
+        if (fence == null)
+        {
+            Debug.LogError("Barrier is not assigned");
+            return;
+        }
+        if (GazeManager.Instance.HitInfo.collider != null && canPlace)
         {
             Debug.Log(GazeManager.Instance.HitInfo.collider.name);
             if (GazeManager.Instance.HitInfo.collider.CompareTag("Path"))
             {
-                Instantiate(barrier, GazeManager.Instance.HitInfo.point, GazeManager.Instance.HitInfo.transform.rotation);
+                if (GameManager.instance.coins > 0) 
+                {
+
+                }
+                //(PlaceItem()); For testing cooldown if needed
+                GameManager.instance.coins -= 100;
+                Instantiate(fence, GazeManager.Instance.HitInfo.point, GazeManager.Instance.HitInfo.transform.rotation);
 
             }
         }
     }
+
+    IEnumerator PlaceItem()
+    {
+        canPlace = false;
+        Debug.Log(canPlace);
+        yield return new WaitForSecondsRealtime(placeCoolDown);
+        canPlace = true;
+        Debug.Log(canPlace);
+    }
 }
+
+
