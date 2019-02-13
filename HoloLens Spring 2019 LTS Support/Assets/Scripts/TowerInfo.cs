15774d656a56a6c8471814969466ab2bab0ed2f5
phunk-tmurph <43594900+phunk-tmurph@users.noreply.github.com>
2019-02-12 22:43:26 -0500

    uh
    yeah sure

---
+++ HoloLens Spring 2019 LTS Support/Assets/Scripts/TowerInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+using UnityEngine.UI;
+
+public class TowerInfo : MonoBehaviour {
+
+    public Text txt;
+    public BaseTower baseTower;
+
+    void OnFocusEnter()
+    {
+        StartCoroutine(showInfo());
+    }
+
+    void OnFocusExit()
+    {
+        StartCoroutine(hideInfo());
+    }
+
+    IEnumerator showInfo()
+    {
+        yield return new WaitForSeconds(2);
+        txt.text = "Damage: " + baseTower.attackDamage + " Radius: " + baseTower.radius + " Fire rate: " + baseTower.attackSpeed;
+    }
+
+    IEnumerator hideInfo()
+    {
+        txt.text = " ";
+        yield return new WaitForSeconds(0);
+    }
+}
