13de68fb57169f34d2a23baa94dd3e9aa50fa76d
phunk-tmurph <43594900+phunk-tmurph@users.noreply.github.com>
2019-02-19 16:14:05 -0500

    sure
    ghfj

---
+++ HoloLens Spring 2019 LTS Support/Assets/Scripts/TowerStats.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+using UnityEngine.UI;
+
+public class TowerStats : MonoBehaviour {
+
+    public Text towerInfo;
+    BaseTower baseTower;
+
+    void Start()
+    {
+        towerInfo.text = "";
+    }
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        towerInfo.text = "Damage: " + baseTower.attackDamage + " Attack Speed: " + baseTower.attackSpeed + " Range: " + baseTower.radius;
+    }
+}
