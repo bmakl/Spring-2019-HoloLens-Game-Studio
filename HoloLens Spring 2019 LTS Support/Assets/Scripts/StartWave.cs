f5a54e97656906b94365bc23846983071544c6b4
phunk-tmurph <43594900+phunk-tmurph@users.noreply.github.com>
2019-02-26 23:46:24 -0500

    wi

--- HoloLens Spring 2019 LTS Support/Assets/Scripts/StartWave.cs
+++ HoloLens Spring 2019 LTS Support/Assets/Scripts/StartWave.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using HoloToolkit.Unity.InputModule;
 
-public class StartWave : MonoBehaviour {
+public class StartWave : MonoBehaviour, IInputClickHandler{
 
     WaveSpawner waveSpawner;
 
 
-    /*public void OnInputClicked(InputClickedEventData eventData)
+    public void OnInputClicked(InputClickedEventData eventData)
     {
-        if(waveSpawner.canStart) //if the wave can start, set canStart to false and begin SpawnWave couroutine
-            StartCoroutine(waveSpawner.SpawnWave());
-    }*/
+        //if(waveSpawner.canStart) //if the wave can start, set canStart to false and begin SpawnWave couroutine
+            //StartCoroutine(waveSpawner.SpawnWave()); idk what happened with wave spawner but theres missing things
+    }
 }
