83fd87f0c79c1c1b5252a4cfd998204b63451748
Ben Rubenstein <benrube4578@gmail.com>
2019-03-09 19:24:38 -0500

    Added tween to start button

---
+++ HoloLens Spring 2019 LTS Support/Assets/StartTweenController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+
+public class StartTweenController : MonoBehaviour, IFocusable
+{
+    [SerializeField] float tweenAmmount;
+    [SerializeField] float tweenDuration;
+
+    private Vector3 originalScale;//Original scale of object before tween
+
+    public void OnFocusEnter()
+    {
+        originalScale = this.transform.localScale;
+        TweenManager.instance.TweenScale(this.transform, tweenAmmount, tweenDuration);
+    }
+
+    public void OnFocusExit()
+    {
+        TweenManager.instance.Descale(this.transform, originalScale, tweenDuration);
+    }
+}
