a3c525a9f53736b170c5ebe5a21c5afb6c19c5ce
Ben Rubenstein <benrube4578@gmail.com>
2019-03-12 15:03:58 -0400

    no message

--- HoloLens Spring 2019 LTS Support/Assets/Scripts/UIUpgrade.cs
+++ HoloLens Spring 2019 LTS Support/Assets/Scripts/UIUpgrade.cs
@@ -4,37 +4,33 @@ using UnityEngine;
 using HoloToolkit.Unity.InputModule;
 using UnityEngine.UI;
 
-public class UIUpgrade : MonoBehaviour, IInputClickHandler
+public class UIUpgrade : MonoBehaviour
 {
 
-    public TowerInfo towerinfo;
-    public TowerTemplate towertemp;
+    /*public TowerInfo towerinfo;
     public BaseTower basetow;
+    public StoreTowerInfo towerI;
+    
     int towerUpNum = 0;
 
     public void OnInputClicked(InputClickedEventData eventData)
     {
-        /* if (towerUpNum == 0)
-         {
-             Debug.Log("bruh 1");
-             basetow.attackDamage = basetow.upgrade1Damage;
-             basetow.radius = basetow.upgrade1Range;
-             basetow.fireRate = basetow.upgrade1fireRate;
-             towerinfo.txt.text = "Damage: " + basetow.attackDamage + " Attack Speed: " + basetow.
-                 + " Range: " + basetow.radius;
-             towerUpNum++;
-         }
+        if (towerUpNum == 0)
+        {
+            towerUpNum++;
+            Debug.Log("bruh 1");
+            basetow.attackDamage = basetow.upgrade1Damage;
+            basetow.radius = basetow.upgrade1Range;
+            basetow.attackSpeed = basetow.upgrade1AttackSpeed;
+        }
 
-         if (towerUpNum == 1) {
-             Debug.Log("bruh 2");
-             basetow.attackDamage = basetow.upgrade2Damage;
-             basetow.radius = basetow.upgrade2Range;
-             basetow.fireRate = basetow.upgrade2fireRate;
-             towerinfo.txt.text = "Damage: " + basetow.attackDamage + " Attack Speed: " + basetow.fireRate + " Range: " + basetow.radius;
-             towerUpNum++;
-         }
-     }
-     */
-
+        if (towerUpNum == 1) {
+            towerUpNum++; //so they cant trigger an upgrade again
+            Debug.Log("bruh 2");
+            basetow.attackDamage = basetow.upgrade2Damage;
+            basetow.radius = basetow.upgrade2Range;
+            basetow.attackSpeed = basetow.upgrade2AttackSpeed;
+        }
     }
+    */
 }
