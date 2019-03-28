5bbdc10d7c8e903aada97d76890a49849c61e00e
bmakl <31875877+bmakl@users.noreply.github.com>
2019-03-27 20:52:34 -0400

    Stuff

---
+++ HoloLens Spring 2019 LTS Support/Assets/Scripts/StartMenuBehaviors.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenuBehaviors : MonoBehaviour {
+
+	public void StartButton()
+    {
+        Debug.Log("Loading PAX Scene");
+        SceneManager.LoadScene(1);
+    }
+
+    public void Instructions()
+    {
+        Debug.Log("Loading Instructions");
+        SceneManager.LoadScene(2);
+    }
+
+    public void Credits()
+    {
+        Debug.Log("Loading Credits");
+        SceneManager.LoadScene(3);
+    }
+
+    public void Settings()
+    {
+        Debug.Log("Loading Settings");
+    }
+}
