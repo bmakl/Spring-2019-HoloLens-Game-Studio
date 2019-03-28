cc906fb5c43c365e0acc5b001593ee82fddb6ad0
bmakl <31875877+bmakl@users.noreply.github.com>
2019-03-27 21:39:34 -0400

    Nameplate Prices

---
+++ HoloLens Spring 2019 LTS Support/Assets/Scripts/PriceNameplates.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PriceNameplates : MonoBehaviour {
+
+    public Text basic;
+    public Text melee;
+    public Text powerful;
+    public Text debuff;
+
+    // Use this for initialization
+    void Start ()
+    {
+
+        if(this.gameObject.CompareTag("ScythePlate"))
+        {
+            melee.text = GameManager.instance.meleeCost.ToString();
+        }
+        if (this.gameObject.CompareTag("TombstonePlate"))
+        {
+            basic.text = GameManager.instance.baseCost.ToString();
+        }
+        if (this.gameObject.CompareTag("HolyWrathPlate"))
+        {
+            powerful.text = GameManager.instance.powerfulCost.ToString();
+        }
+        if (this.gameObject.CompareTag("CursedPlate"))
+        {
+            debuff.text = GameManager.instance.debuffCost.ToString();
+        }
+
+    }
+	
+}
