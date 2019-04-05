148ba27072f4839eab5271f29f6f34c0741fede3
a <a>
2019-04-05 17:30:49 -0400

    Created MapRandom Script

---
+++ HoloLens Spring 2019 LTS Support/Assets/Scripts/MapRandom.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapRandom : MonoBehaviour
+{
+    public Transform spawnPoint;
+
+    public GameObject[] prefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(prefab[Random.Range(0, 4)], spawnPoint.position, Quaternion.identity);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
